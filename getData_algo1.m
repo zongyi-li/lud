9cf853b26e733eb938784c194d2248f0ebdbca65
wumming <johnnygodspood@gmail.com>
2018-05-02 16:56:37 -0500

    run_able listreg

--- getData_algo1.m
+++ getData_algo1.m
@@ -4,26 +4,27 @@ m = 10; % number of terms
 i_m = 5;
 d = 4; % dim of y
 
-% y part of data
-datay = 2*rand(m, d) - 1; % y is uniformly dist
-% w for the optimal linear fit
-w = ones(d,1);
-
-noise = normrnd(0,0.2,i_m,1);
-
-% z part of data
-dataz = zeros(m,1);
-% first 1:10 terms are Igood, z = Y * w + noise
-dataz(1:i_m,1) = datay(1:i_m,1:d) * w + noise;
-% the rest of terms are random
-dataz(i_m+1:end,1) = 2*rand(m-i_m,1) - 1;
+% % y part of data
+% datay = 2*rand(m, d) - 1; % y is uniformly dist
+% % w for the optimal linear fit
+% w = ones(d,1);
+% 
+% noise = normrnd(0,0.2,i_m,1);
+% 
+% % z part of data
+% dataz = zeros(m,1);
+% % first 1:10 terms are Igood, z = Y * w + noise
+% dataz(1:i_m,1) = datay(1:i_m,1:d) * w + noise;
+% % the rest of terms are random
+% dataz(i_m+1:end,1) = 2*rand(m-i_m,1) - 1;
 
 % T is the number of points (weights) for each term.
 T = ones(m,1);
 
 
-LossMat = ones(d+1,d+1,m);
-shift = ones(1,d)
-[ws_v, Y_v, c] = quadratic(LossMat, T, shift);
-
+lossMat = ones(d+1,d+1,m);
+shift = ones(1,d);
+%[ws_v, Y_v, c] = quadratic(lossMat, T, shift);
 %[c_p] = updateWeights(c, ws_v, datay, dataz, T, Y_v);
+mu = 0.1; r0 = 100; rfinal = 1;
+[U, W] =  listreg(T, lossMat, mu, r0, rfinal);
--- listreg.m
+++ listreg.m
@@ -1,44 +1,47 @@
-function [U, W] =  listreg(T, lossMat, delta, mu, r0, rfinal)
+function [U, W] =  listreg(T, lossMat, mu, r0, rfinal)
 % T: number of examples in each term
 % lossMat: (d+1)*(d+1)*m loss matrix, example info to compute the l2 loss
 % ws: m*d
 
-[ws_v, ~, ~] =  quadratic(lossMat, T); % first run
 m = size(T,1);
+d = size(lossMat,1) - 1;
 N = sum(T);
 r = r0;
 
-t_maxiter = 100;
+[ws_v, ~, ~] =  quadratic(lossMat, T , zeros(1,d) ); % first run, zero shift
+
+t_maxiter = 10;
 for t=1:t_maxiter
     W = ws_v;
     if r < 1/2*rfinal  % terminating case
-        [U, W] = greedycluster(W,rfinal);
+        U = r; % U = greedycluster(W,rfinal);
         break;
     end
-    
+
+    padded_maxiter = 10;  % 112*log(t*(t+1)/delta);
     Wh = NaN(m,d,padded_maxiter);
     delta_p = 7/8;
     rho = r*log(2/mu)/delta_p;
     tau = 2*r;
-    padded_maxiter = 10;  % 112*log(t*(t+1)/delta);
+
     for h=1:padded_maxiter
         [P,~,Cr] = padded(W, rho, tau); % P: partition, each cell is a cluster
         for i=1:size(P,2)               % for each cluster
             size_T = sum(T(P{i}.index)); % number of pt in this cluster
             if size_T > mu * N              % Run algo1 
-                shift = Cr{h}(1:end-1);
+                shift = Cr{i}(1:end-1);
                 clusterlossMat = lossMat(:,:,P{i}.index); 
                 [ws_cluster, ~, ~] =  quadratic(clusterlossMat, T(P{i}.index), shift);
-                Wh(h,P{i}.index,:) = ws_cluster;
+                Wh(P{i}.index,:,h) = ws_cluster;
             end
         end
     end
     for i=1:m
         for h0 = 1:padded_maxiter
             % dimension: may need to transpose
-            D = sqrt(sum((Wh(:,i,:) - repmat(Wh(h0,i,:),padded_maxiter,1)).^2));
-            if sum(D <= 1/3*r) > 1/2*padded_maxiter
-                ws_v(i,:) = Wh(h0,i,:);
+            D = sqrt(sum((Wh(i,:,:) - repmat(Wh(i,:,h0), 1,1,padded_maxiter)).^2, 2));
+            if sum(D <= 1/3*r) > 1/2 * padded_maxiter
+                ws_v(i,:) = Wh(i,:,h0);
                 break;
             end
         end
